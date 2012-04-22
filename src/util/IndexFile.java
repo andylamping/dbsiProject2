f1aae746172d98a2efb1ec721416f17cb03db263
andylamping <andrewlamping@gmail.com>
2012-04-22 19:07:58 -0300

    Update src/util/IndexFile.java

--- src/util/IndexFile.java
+++ src/util/IndexFile.java
@@ -328,7 +328,9 @@ public class IndexFile {
 			// add all elements from the overflow bucket
 			while ( index < overflowBucket.getCurrentSize()){
 				Object pluck = overflowBucket.data[index][0];
+				Object ptr = overflowBucket.data[index][1];
 				currentContents.add(pluck);
+				currentContents.add(ptr);
 				System.out.println("Overflow item " + pluck);
 				index++;
 			}
@@ -350,7 +352,7 @@ public class IndexFile {
 			int hash = getHash(data);
 			System.out.println("New hash " + hash);
 			this.writeToIndexFile(data, 0);
-			index++;	
+			index = index + 2;	
 		}
 		
 		
